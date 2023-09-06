ac228852fa93cf83b4a91514b257f2efbbbd9239
Chris Walter <walt50@gmail.com>
2023-09-05 20:06:11 -0400

    Initial Commit

---
+++ InstagramTutorial/InstagramTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramTutorialApp.swift
+//  InstagramTutorial
+//
+//  Created by Christopher Walter on 9/5/23.
+//
+
+import SwiftUI
+
+@main
+struct InstagramTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
